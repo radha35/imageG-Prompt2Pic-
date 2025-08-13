1749cc8996b2117c3ff0e8e0a4e68bb7fdfec3af
Radha <chittalwarradha@gmail.com>
2025-08-13 23:07:36 +0530

    Initial commit - hide all .env files

---
+++ server/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    mongoose.connection.on('connected',()=>{
+        console.log("Database Connected Successfully")
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/ImageG`)
+}
+
+export default connectDB;
